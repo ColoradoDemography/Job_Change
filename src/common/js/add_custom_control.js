76f768ae2aa16e23da37493ca8549695bb4d15aa
Bleess <TBleess@dola.state.co.us>
2024-10-31 12:04:47 -0600

    2023

--- src/common/js/add_custom_control.js
+++ src/common/js/add_custom_control.js
@@ -100,7 +100,7 @@ module.exports = function(map: Object, layer: Object, worker_data: any) {
 
         refreshdata(layer, main_data);
     } else {
-        let e: any = document.querySelector('#selto [value="2022"]');
+        let e: any = document.querySelector('#selto [value="2023"]');
         e.selected = true;
         refreshdata(layer, main_data);
 
